62ee29ff3eea357df07237c849b3953a8b4fa647
David Malone <dmalone@pivotal.io>
2017-07-12 08:42:41 -0400

    fixing broken test due to removal of text from homepage header

--- src/views/HomepageHeader.test.js
+++ src/views/HomepageHeader.test.js
@@ -4,6 +4,4 @@ import HomepageHeader from './HomepageHeader'
 
 it('renders without crashing', () => {
   const wrapper = shallow(<HomepageHeader />)
-  expect(wrapper.contains(<h2 className="homepage-header-title">Know what your child is learning</h2>)).toBeTruthy()
-  expect(wrapper.contains(<h4 className="homepage-header-subtitle">A video library and support platform by Teachers for You</h4>)).toBeTruthy()
 });
