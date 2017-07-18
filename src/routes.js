c453d740f2ae59583cf5b6c18726074f74b5d868
David Malone <dmalone@pivotal.io>
2017-07-18 19:00:50 -0400

    added proper handling for not found pages

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { Route } from 'react-router'
+import { Route, Switch } from 'react-router'
 import ReactGA from 'react-ga'
 import { ConnectedRouter } from 'react-router-redux'
 import configureStore from './store/configureStore'
@@ -41,16 +41,16 @@ const UserProfileDashboard = () =>
 const Routes = () => (
   <Provider store={store}>
     <ConnectedRouter history={history} handleLocationChange={logPageView}>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/callback" component={Callback} />
-        <Route path="/login" component={Login} />
-        <Route path="/privacy" component={PrivacyPolicy} />
-        <Route path="/logout" component={RequiresAuth(Logout)} />
-        <Route path="/dashboard" component={RequiresAuth(EarlyRegistrationDashboard)} />
-        <Route path="/profile" component={RequiresAuth(UserProfileDashboard)} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/callback' component={Callback} />
+        <Route path='/login' component={Login} />
+        <Route path='/privacy' component={PrivacyPolicy} />
+        <Route path='/logout' component={RequiresAuth(Logout)} />
+        <Route path='/dashboard' component={RequiresAuth(EarlyRegistrationDashboard)} />
+        <Route path='/profile' component={RequiresAuth(UserProfileDashboard)} />
         <Route component={PageNotFound}/>
-      </div>
+      </Switch>
     </ConnectedRouter>
   </Provider>
 )
