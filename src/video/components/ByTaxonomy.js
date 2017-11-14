dcfdf9ef2fb4dad80b103baab06205d968b8c45e
David Malone <avidmalone@gmail.com>
2017-11-14 14:40:07 -0500

    added messaging for unavailable grades

--- src/video/components/ByTaxonomy.js
+++ src/video/components/ByTaxonomy.js
@@ -66,104 +66,115 @@ export default class ByTaxonomy extends React.Component {
           {title}
         </PageHeader>
 
-        <Grid fluid style={styles.containerFluid}>
-          <Row style={styles.videoRow}>
-            <Col style={styles.videoGroupTitle}>
-              Recently Viewed
-            </Col>
-          </Row>
-          <Row style={styles.videoRow}>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-          </Row>
-          <Row style={styles.videoRow}>
-            <Col style={styles.videoGroupTitle}>
-              Unit > Set
-            </Col>
-          </Row>
-          <Row style={styles.videoRow}>
-            <Col style={styles.videoGroupTitle}>
-              Unit > Set
-            </Col>
-          </Row>
-          <Row style={styles.videoRow}>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-            <Col sm={3} style={styles.videoCell}>
-              <Link to="">
-                <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
-                 <h5>Video Title</h5>
-               </Thumbnail>
-             </Link>
-            </Col>
-          </Row>
-        </Grid>
+        { grade !== '3rd' &&
+          <div>
+            <p>Thank you for signing up!</p>
+
+            <p>We currently do not have videos for {title}, but we're working on releasing those videos soon. You will not be charged for students registered in {title} at this time.</p>
+
+            <p>Once the {title} videos are available, we will send an email notification and provide a 14 day free trial for you to explore our service.</p>
+          </div>
+        }
+        { grade === '3rd' &&
+          <Grid fluid style={styles.containerFluid}>
+            <Row style={styles.videoRow}>
+              <Col style={styles.videoGroupTitle}>
+                Recently Viewed
+              </Col>
+            </Row>
+            <Row style={styles.videoRow}>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+            </Row>
+            <Row style={styles.videoRow}>
+              <Col style={styles.videoGroupTitle}>
+                Unit > Set
+              </Col>
+            </Row>
+            <Row style={styles.videoRow}>
+              <Col style={styles.videoGroupTitle}>
+                Unit > Set
+              </Col>
+            </Row>
+            <Row style={styles.videoRow}>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+              <Col sm={3} style={styles.videoCell}>
+                <Link to="/video/1234">
+                  <Thumbnail src="https://i.ytimg.com/vi/VzAap35yd34/maxresdefault.jpg" alt="Video Title">
+                   <h5>Video Title</h5>
+                 </Thumbnail>
+               </Link>
+              </Col>
+            </Row>
+          </Grid>
+        }
       </div>
     )
   }
